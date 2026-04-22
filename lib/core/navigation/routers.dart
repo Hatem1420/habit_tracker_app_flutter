bfb4ee84abe0b6900186efc8b9f05e1b7eb69340
hatem1420 <hatembatayah30@gmail.com>
2026-04-23 02:11:55 +0300

    latest files

--- lib/core/navigation/routers.dart
+++
@@ -1,7 +0,0 @@
-class Routes {
-  Routes._();
-  static const String habits = '/habits';
-  static const String auth = '/auth';
-  static const String splash = '/splash';
-  static const String habitLogs = '/habitLogs';
-}
--- lib/features/splash/domain/repositories/splash_repository_domain.dart
+++
@@ -1,6 +0,0 @@
-import 'package:multiple_result/multiple_result.dart';
-import 'package:personal_habit_tracker_app/core/errors/failure.dart';
-
-abstract class SplashRepositoryDomain {
-    Future<Result<bool, Failure>> getSplash();
-}
